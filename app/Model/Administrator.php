4cb5e089f9988ee674871f653607166d4f73b3de
maricelmonge <marimong25@gmail.com>
2016-05-22 23:16:18 +0000

    Comentarios del modelo de Administrator

--- app/Model/Administrator.php
+++ app/Model/Administrator.php
@@ -1,13 +1,18 @@
 <?php
 App::uses('AppModel', 'Model');
+
 /**
  * Administrator Model
+ * 
+ * Modelo que contiene las validaciones de los campos de Administrator y sus relaciones con los otros modelos.
  *
  * @property User $User
  * @property Aboutus $Aboutus
  * @property Download $Download
  * @property Link $Link
  */
+
+ 
 class Administrator extends AppModel {
 
 /**
@@ -16,6 +21,7 @@ class Administrator extends AppModel {
  * @var array
  */
 	public $validate = array(
+		//Verifica que el campo de especialidad no este vacío.
 		'specialty' => array(
 			'notBlank' => array(
 				'rule' => array('notBlank'),
@@ -27,6 +33,7 @@ class Administrator extends AppModel {
 			),
 		),
 		'curriculum' => array(
+			//Verifica que el campo de curriculum no este vacío.
 			'notBlank' => array(
 				'rule' => array('notBlank'),
 				//'message' => 'Your custom message here',
@@ -41,7 +48,7 @@ class Administrator extends AppModel {
 	// The Associations below have been created with all possible keys, those that are not needed can be removed
 
 /**
- * belongsTo associations
+ * Relación con el modelo de User
  *
  * @var array
  */
@@ -56,7 +63,7 @@ class Administrator extends AppModel {
 	);
 
 /**
- * hasMany associations
+ * Otras relaciones con el de modelo de Administrator
  *
  * @var array
  */
